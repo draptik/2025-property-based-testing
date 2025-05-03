cc16e9f89baff073f79d3974dd016ee23f70b594
Patrick Drechsler <patrick.drechsler@mathema.de>
2025-05-03 16:16:27 +0200

    ✨ chore(code): prepare hello world examples

---
+++ code/PBTDemos/tests/HelloWorld.Tests/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld.Tests;
+
+public class CalculatorTests
+{
+  private static int Add(int a, int b)
+  {
+    return 0;
+  }
+
+  [Fact]
+  public void Add_1_and_2_returns_3()
+  {
+    var actual = Add(1, 2);
+    var expected = 3;
+    Assert.Equal(expected, actual);
+  }
+}
